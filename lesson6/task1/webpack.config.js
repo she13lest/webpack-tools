8064f39de4c51a8b44f22267f4c0dfd3d03626df
Anna <she13lest@gmail.com>
2022-07-12 11:45:39 +0200

    new

---
+++ lesson6/task1/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /.(jpg|png)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
