94bf293d42348f29cbe76d8409d79b1b80d6ae98
Anna <she13lest@gmail.com>
2022-07-12 13:42:36 +0200

    new

--- lesson6/task1/webpack.config.js
+++ lesson6/task1/webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /.s?.css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /.(jpg|png)$/,
@@ -16,6 +16,8 @@ module.exports = {
             loader: "url-loader",
             options: {
               limit: 8192,
+              name: "[name].[ext]",
+              outputPath: "images",
             },
           },
         ],
